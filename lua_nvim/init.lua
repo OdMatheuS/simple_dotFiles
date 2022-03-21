6a5317eb52a3066da7b477ccd4444188621510ff
Matheus N <56494951+OdMatheuS@users.noreply.github.com>
2022-03-20 21:47:58 -0300

    feat:little setup telescope+remaps

--- lua_nvim/init.lua
+++ lua_nvim/init.lua
@@ -1,70 +1,100 @@
 require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-  
-    -- Colorscheme
-    use 'ellisonleao/gruvbox.nvim'
-  
-    --Line
-    use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-  
-    --Format
-    use 'sbdchd/neoformat'
-  
-    -- LSP
-    use {
-      'VonHeikemen/lsp-zero.nvim',
-      requires = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},
-        {'williamboman/nvim-lsp-installer'},
-  
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},
-        {'hrsh7th/cmp-buffer'},
-        {'hrsh7th/cmp-path'},
-        {'saadparwaiz1/cmp_luasnip'},
-        {'hrsh7th/cmp-nvim-lsp'},
-        {'hrsh7th/cmp-nvim-lua'},
-  
-        -- Snippets
-        {'L3MON4D3/LuaSnip'},
-        {'rafamadriz/friendly-snippets'},
-      }
-    }
-  end)
-  
-  -- LSP setup
-  local lsp = require('lsp-zero')
-  lsp.preset('recommended')
-  lsp.nvim_workspace()
-  lsp.setup()
-  -- Tab set to two spaces
-  vim.opt.tabstop = 2
-  vim.opt.shiftwidth = 2
-  vim.opt.softtabstop = 2
-  vim.opt.expandtab = true
-  vim.opt.signcolumn = 'yes'
-  
-  --esse comando serve para numerar as linhas--
-  vim.opt.number = true
-  
-  --faz o calculo da distancia das linhas--
-  vim.opt.relativenumber = true
-  --destaque nos resultados--
-  vim.opt.hlsearch = true
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-  --busca incremental - traz feedback enquanto busco--
-  vim.opt.incsearch = true
+  -- Colorscheme
+  use 'ellisonleao/gruvbox.nvim'
 
-  vim.opt.termguicolors = true
-  vim.o.background = "dark" -- or "light" for light mode
-  pcall(vim.cmd, 'colorscheme gruvbox')
+  --Line
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+  --Telescope
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  --Format
+  use 'sbdchd/neoformat'
+
+  -- LSP
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/nvim-lsp-installer'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
+end)
+
+-- LSP setup
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+lsp.nvim_workspace()
+lsp.setup()
+-- Tab set to two spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.signcolumn = 'yes'
+
+--esse comando serve para numerar as linhas--
+vim.opt.number = true
+
+--faz o calculo da distancia das linhas--
+vim.opt.relativenumber = true
+
+--destaque nos resultados--
+vim.opt.hlsearch = true
+
+--busca incremental - traz feedback enquanto busco--
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+vim.o.background = "dark" -- or "light" for light mode
+pcall(vim.cmd, 'colorscheme gruvbox')
+
 
 -- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.api.nvim_set_keymap('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
+--Template pra chamar fn como se tivesse chamando igual no command default do Vim
+--vim.api.nvim_set_keymap('n', ';f', ':%s/foo/bar/gc <CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ';f', ':Telescope find_files <CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ';p', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ';k', ':Telescope file_browser<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ';j', ':Telescope current_buffer_fuzzy_find<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'za', ':so%<CR>', { noremap = true, silent = true })
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
@@ -79,7 +109,15 @@ require('packer').startup(function(use)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gt', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+  if client.resolved_capabilities.document_formatting then
+    vim.api.nvim_command [[augroup Format]]
+    vim.api.nvim_command [[autocmd! * <buffer>]]
+    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+    vim.api.nvim_command [[augroup END]]
+  end
 end
+
 
 require('lualine').setup {
   options = {
@@ -111,3 +149,35 @@ require('lualine').setup {
   tabline = {},
   extensions = {}
 }
+
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = "which_key"
+      }
+    }
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  }
+}
+
